9e4961ffda278b8c938f477b1e46edbd20290119
galongatti <galongatti@hotmail.com>
2025-09-13 16:09:10 -0300

    project beginning

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ManagerPeople.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
